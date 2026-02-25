d5926ed1300ec55b3a6f6cb1ff058fdba8897f76
vedant gavate <you@example.com>
2026-02-25 18:29:44 +0530

    Integrated Spring Data JPA and Repository Layer

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,11 +1,16 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    TodoRepository todoRepository;
     @GetMapping("/hello")
     public String getHello(){
         return "<b>Hello World!!!</b>";
@@ -13,6 +18,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String getHome(){
+        List<Todo> todos = todoRepository.findAll();
         return "Welcome Home";
     }
 
---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    Integer id ;
+
+    @Column
+    String title;
+
+    @Column
+    Boolean completed;
+}
